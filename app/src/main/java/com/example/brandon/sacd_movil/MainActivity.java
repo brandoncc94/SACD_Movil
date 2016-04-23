905ad4bddb147444ae1fb49276081feb429ced80
brandoncc94 <brandoncc94@gmail.com>
2016-04-23 15:26:15 -0600

    Buscar listo

--- app/src/main/java/com/example/brandon/sacd_movil/MainActivity.java
+++ app/src/main/java/com/example/brandon/sacd_movil/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.brandon.sacd_movil;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
@@ -8,6 +9,7 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +22,15 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Inicio");
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#27AE8D"));
         getSupportActionBar().setBackgroundDrawable(colorDrawable);
-        //Typeface type = Typeface.createFromAsset(getAssets(),"fonts/Kokila.ttf");
-        //TextView lblElegir = (TextView) this.findViewById(R.id.lbl_elegir);
-        //lblElegir.setTypeface(type);
+
+        ImageButton btnBuscar = (ImageButton)findViewById(R.id.imgSearch);
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BusquedaProfesor.class);
+                startActivity(intent);
+            }
+        });
     }
 }

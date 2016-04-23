b1152a7d6e182cfe413a8cd10fe8d95448795fbe
brandoncc94 <brandoncc94@gmail.com>
2016-04-23 13:30:07 -0600

    Pantalla principal

--- app/src/main/java/com/example/brandon/sacd_movil/MainActivity.java
+++ app/src/main/java/com/example/brandon/sacd_movil/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.brandon.sacd_movil;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        //Prueba de comentario
-        //fabiola
-        //GCP
+        setTitle("Inicio");
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#27AE8D"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+        //Typeface type = Typeface.createFromAsset(getAssets(),"fonts/Kokila.ttf");
+        //TextView lblElegir = (TextView) this.findViewById(R.id.lbl_elegir);
+        //lblElegir.setTypeface(type);
     }
 }
